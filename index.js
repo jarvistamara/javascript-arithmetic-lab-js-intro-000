b824ac08010db1cac2b1dddb03f8a4ead6113995
Tamara Jarvis <tamara.jarvis17@icloud.com>
2020-12-16 15:50:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  var add = a + b;
+  return add;
+}
+
+function subtract(a,b) {
+  var subtract = a - b;
+  return subtract;
+}
+
+function multiply(a,b) {
+  var multiply = a * b;
+  return multiply;
+}
+
+function divide(a,b) {
+  var divide = a / b;
+  return divide;
+}
+
+function increment(n) {
+  var n = ++n;
+  return n;
+}
+
+function decrement(n) {
+  var n = --n;
+  return n;
+}
+
+function makeInt(n) {
+  parseInt('n', 10);
+  return n;
+}
+
+function preserveDecimal(n) {
+  var n = parseFloat(n);
+  return n;
+}
